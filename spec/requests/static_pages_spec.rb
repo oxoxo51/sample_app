a1261a10e6fb2fbf5444b43b23c08abca91795a5
oxoxo51 <y.omt0601@gmail.com>
2015-07-22 23:56:45 +0900

    Finish Chap.3 exam.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
 describe "Static pages" do
+  
+  let(:base_title) {"Ruby on Rails Tutorial Sample App"}
+  
 
   describe "Home page" do
 
@@ -11,7 +14,7 @@ describe "Static pages" do
     
     it "should have the right title" do
       visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+      expect(page).to have_title("#{base_title} | Home")
     end
     
   end
@@ -25,7 +28,7 @@ describe "Static pages" do
     
     it "should have the right title" do
       visit '/static_pages/help'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+      expect(page).to have_title("#{base_title} | Help")
     end
     
   end
@@ -39,10 +42,23 @@ describe "Static pages" do
     
     it "should have the right title" do
       visit '/static_pages/about'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About")
+      expect(page).to have_title("#{base_title} | About")
     end
     
   end
   
+  describe "Contact page" do
+    
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+    
+    it "should have the right title" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{base_title} | Contact")
+    end
+    
+  end
   
 end
