f7d24d78e4b26d3779bfba68128d699ed471bb04
oxoxo51 <y.omt0601@gmail.com>
2015-08-17 22:09:14 +0900

    Finish user edit, update, index, and destroy actions

--- Gemfile
+++ Gemfile
@@ -5,9 +5,11 @@ ruby '2.0.0'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.1'
-
 gem 'bootstrap-sass', '2.3.2.0'
 gem 'bcrypt-ruby', '3.1.2'
+gem 'faker', '1.1.2'
+gem 'will_paginate', '3.0.4'
+gem 'bootstrap-will_paginate', '0.0.9'
 
 # Use sqlite3 as the database for Active Record
 gem 'sass-rails', '~> 5.0'
@@ -39,7 +41,7 @@ end
 
 group :development, :test do
   gem 'sqlite3', '1.3.8'
-  gem 'rspec-rails', '2.13.1'
+  gem 'rspec-rails', '~> 2.14.0.rc1'
   gem 'guard', '2.6.1'
   gem 'guard-rspec', '2.5.0'
     # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,9 +10,10 @@ describe User do
   it {should respond_to(:password_digest)}
   it {should respond_to(:password_confirmation)}
   it {should respond_to(:authenticate)}
-  it {should respond_to(:authenticate)}
+  it {should respond_to(:admin)}
   
   it {should be_valid}
+  it {should_not be_admin}
   
   describe "when name is not present" do
     before {@user.name=""}
@@ -96,6 +97,15 @@ describe User do
   describe "remember token" do
     before {@user.save}
     its(:remember_token) {should_not be_blank}
+  end
+  
+  describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+    
+    it {should be_admin}
   end
   
   
